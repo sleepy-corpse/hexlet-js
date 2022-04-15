78b425ed8b896704328bf76c3a3e3d193df8a9ae
Rodion Svinuhov <rodion2014@gmail.com>
2022-04-16 00:09:15 +0300

    added prettier

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import _ from 'lodash';
-console.log('My first file created through VScode in WSL');
-console.log(_.last(['first', 'last']));
+import _ from "lodash";
+console.log("My first file created through VScode in WSL");
+console.log(_.last(["first", "last"]));
